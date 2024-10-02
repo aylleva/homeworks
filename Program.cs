641e2ecb967ec3a5a5367945a7ef3ea2e9c2266d
aylleva <147303116+aylleva@users.noreply.github.com>
2024-10-03 00:50:57 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//task1
+Console.WriteLine("2 mertebeli ededi daxil edin:");
+int num = Convert.ToInt32(Console.ReadLine());
+int a, b;
+
+a = num % 10;
+b = num / 10;
+if(a>b)
+{
+    int max = a;
+    Console.WriteLine(max);
+}
+if (b > a)
+{
+    int max = b;
+    Console.WriteLine(max);
+}
